7ff914013fce1659b8f14a5549310de83b9bb0ae
Abedkhurin <35690172+Abedkhurin@users.noreply.github.com>
2018-07-12 19:13:56 +0700

    JS

---
+++ depan/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+		loop:false,
+		margin:10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:false
+			},
+		}
+	})
+});
